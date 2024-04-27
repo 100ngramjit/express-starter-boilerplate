716e5bd643ec1d68b0182502caef389069120c56
100ngram <sangramjitdutta@gmail.com>
2024-04-27 21:58:31 +0530

    feat: added json formatting

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const PORT = 8080;
 
 app.use(express.json());
@@ -6,23 +7,21 @@ app.use(express.json());
 app.listen(PORT, () => console.log(`its alive on http://localhost:${PORT}`));
 
 app.get("/helloworld", (req, res) => {
-  res.status(200).send({
+  res.json({
     msg: "Hello World!",
     category: "Noob",
   });
 });
 
 app.get("/", (req, res) => {
-  res.status(200).send({
-    msg: "Welcome to the API Bitch",
+  res.json({
+    msg: "Welcome to the API",
   });
 });
 
 app.get("/:id", (req, res) => {
   const { id } = req.params;
-  res.status(200).send({
-    status: 200,
-    msg: `This is a very beginner level api , the ID passed is : ${id}`,
-    ID: Number(id),
+  res.json({
+    msg: `This is the ID : ${id}`,
   });
 });
