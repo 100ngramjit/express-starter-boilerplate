bbe6c60ed8849e51170e08aa864d92deae0e42b3
100ngram <sangramjitdutta@gmail.com>
2024-04-28 21:22:02 +0530

    feat: add middleware

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+//imports and config
+
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 3001;
 
 app.use(express.json());
 
+//port listener
+
 app.listen(PORT, () => console.log(`its alive on http://localhost:${PORT}`));
 
-app.get("/helloworld", (req, res) => {
+//middleware
+
+function userNameMiddleware(req, res, next) {
+  const username = req.headers["username"];
+
+  if (!username || username.length <= 3) {
+    res.status(404).json({
+      status: "404",
+      msg: "Invalid Username/ Username not found",
+    });
+  } else {
+    next();
+  }
+}
+
+function idValidatorMiddleware(req, res, next) {
+  const id = req.params.id;
+  if (isNaN(id) || Number(id) > 100) {
+    res.json({
+      msg: "invalid id",
+    });
+  } else {
+    next();
+  }
+}
+
+//routes
+
+app.get("/helloworld", userNameMiddleware, (req, res) => {
   res.json({
-    msg: "Hello World!",
+    msg: `Hello ${req.headers["username"]}!`,
     category: "Noob",
   });
 });
@@ -19,7 +51,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/:id", (req, res) => {
+app.get("/:id", idValidatorMiddleware, (req, res) => {
   const { id } = req.params;
   res.json({
     msg: `This is the ID : ${id}`,
